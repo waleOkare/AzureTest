c7aa23d48597f233189e5822fdc2729288553949
waleokare <waleokare@outlook.com>
2016-03-07 10:50:44 +0000

    0

--- week6/index.php
+++ week6/index.php
@@ -31,10 +31,10 @@
     if($age >= 16) {
         print "specs";
     }
-    elseif ($age>=18) {
+    elseif($age>=18) {
         print "mugs";
     }
-        elseif ($age >= 21)
+        elseif($age >= 21)
         {
           print "sausage rolls ";
     }
