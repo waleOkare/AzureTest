20a951e8b836cf1c8a5174acde5969112c705d48
waleokare <waleokare@outlook.com>
2016-03-07 10:26:42 +0000

    0

--- week6/index.php
+++ week6/index.php
@@ -8,11 +8,11 @@
   //  echo "Hello Worrld! <br>";
 
     echo"Hello," . " " . "world" . "! <br>";
-    echo 5 * 7;
+   // echo 5 * 7;
 
     $myname = "Wale Patrick";
     $myage = 30;
-    echo "My name is" . $myname . "and I am " . $myage;
+    echo "My name is " . " "  . $myname . "  " . "and I am " . $myage . " Years old ";
     ?>
 </p>
 </body>
