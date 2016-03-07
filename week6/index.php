1edd733031dcaecf537d500c242de8ba36482583
waleokare <waleokare@outlook.com>
2016-03-07 10:33:16 +0000

    0

--- week6/index.php
+++ week6/index.php
@@ -4,7 +4,10 @@
 </head>
 <body>
 <p>
+
     <?php
+    // section 3
+
   //  echo "Hello Worrld! <br>";
 
     echo"Hello," . " " . "world" . "! <br>";
@@ -13,6 +16,16 @@
     $myname = "Wale Patrick";
     $myage = 30;
     echo "My name is " . " "  . $myname . "  " . "and I am " . $myage . " Years old ";
+
+    $name = "Patrick";
+
+    if($name == "wale"){
+        print "I know you!";
+    }
+    else {
+        print "Who are you";
+    }
+
     ?>
 </p>
 </body>
