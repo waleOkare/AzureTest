b693ad48517b7ab38d43c0d2e0f28dbd962b7547
waleokare <waleokare@outlook.com>
2016-03-15 09:50:14 +0000

    week5 work

---
+++ week5/home.php
@@ -0,0 +1,13 @@
+<?php
+<head>
+<meta charset="utf-8">
+    <title> Welcome </title>
+
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>Hello</h1>
+</body>
+</html>
+
+?>
