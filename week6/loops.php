d4f192e2fa8c333646db77448d26888500145383
waleokare <waleokare@outlook.com>
2016-03-16 19:17:49 +0000

    week6 work

--- week6/loops.php
+++ week6/loops.php
@@ -6,11 +6,12 @@ $items = array("specs", "mugs", "Sausage rolls");
 
  // echo $items[0];
 
-for($i = 1; $i <=30; $i++){
+ ///for($i = 1; $i <=30; $i++){
 
   //  echo "ont the " . $i . "of the month no products are available ";
-for($j = 1; $j <=30; $j+2)
+for($j = 1; $j <=30; $j+2){
     echo " On the " . $i . "of the month" .$items[0] . "are available ";
+
 }
 ?>
 
