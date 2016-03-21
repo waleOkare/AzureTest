a9b1c64fc0740c9fd636c4e3d7d9b2e0754fa8d2
waleokare <waleokare@outlook.com>
2016-03-21 18:33:27 +0000

    from home3

--- week7a/formResponse.php
+++ week7a/formResponse.php
@@ -2,12 +2,15 @@
 
 $forename = $_POST["forename"];
 $surname = $_POST["surname"];
-if($forename && $surname !=empty($_POST)){
+
+/*  If both fields are Left empty, form will echo both field required.  */
+if($forename || $surname !=empty($_POST)){
 
     echo "Hello " . $forename . " " . $surname;
 }else
 
 echo "both field required";
+
 
 
 ?>
