6361439fc575c859827a0468755e59f3cdbc3732
waleokare <waleokare@outlook.com>
2016-03-21 18:28:03 +0000

    from home3

--- week7a/formResponse.php
+++ week7a/formResponse.php
@@ -2,7 +2,12 @@
 
 $forename = $_POST["forename"];
 $surname = $_POST["surname"];
+if($forename && $surname !=empty($_POST)){
 
-echo "Hello" . $forename . " " . $surname;
+    echo "Hello " . $forename . " " . $surname;
+}else
+
+echo "both field required";
+
 
 ?>
