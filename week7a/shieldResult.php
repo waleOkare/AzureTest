a5d818314bb390bc36796881419b49839cc88cef
waleokare <waleokare@outlook.com>
2016-03-21 19:12:35 +0000

    from home3

--- week7a/shieldResult.php
+++ week7a/shieldResult.php
@@ -3,7 +3,7 @@
 $firstname = $_POST["firstname"];
 $surname = $_POST["surname"];
 
-$sex = $_POST["male"] || $_POST["female"];
+$sex = $_POST["male"] && $_POST["female"];
 
 echo $firstname . " " . $surname . " <br>";
 
