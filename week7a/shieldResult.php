ea31a4df7d1b04942b1621edff0d16d70b0dd049
waleokare <waleokare@outlook.com>
2016-03-21 19:39:31 +0000

    from home3

--- week7a/shieldResult.php
+++ week7a/shieldResult.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title> S.H.I.E.L.D </title>
+</head>
+
+<body bgcolor="#6b8e23 "></body>
+
+
+</html>
 <?php
 
 $firstname = $_POST["firstname"];
@@ -14,5 +25,5 @@ echo "Gender: " . $gender. " <br> ";
 
 echo "Main Superhero Power:  " . $power;
 
-
+?>
 
