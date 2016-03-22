59cda5fe74a02c2ffe30d060f765dcb21f211cb1
waleokare <waleokare@outlook.com>
2016-03-22 23:31:30 +0000

    from home3

--- week7b/showsuperheros.php
+++ week7b/showsuperheros.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>superheroform</title>
+</head>
+<body BGCOLOR="#F5FFFA">
+<header style="background-color: darkgreen">
+    <h1 style="color: silver" align="center"> SUPERHERO FORM  </h1>
+</header>
+
+
+<? include ("dbconnection.php"); ?>
+
 <?php
 
 
