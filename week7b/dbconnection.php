6f90a41dc91f4cfe31d8d436c7c72046a2ab2ba4
waleokare <waleokare@outlook.com>
2016-03-22 23:01:47 +0000

    from home3

--- week7b/dbconnection.php
+++ week7b/dbconnection.php
@@ -11,8 +11,16 @@ if($db->connect_errno){
 }
 
 
+$sql_query = "SELECT * FROM superheros WHERE superpower LIKE '%laser%'";
+$result = $db->query($sql_query);
 
+while($row = $result->fetch_array()){
 
+    echo "<p>" . $row['superheroName'] . "</p>";
+}
+
+$result->close();
+$db->close();
 
 
 
---
+++ week7b/insertsuperhero.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>superheroform</title>
+</head>
+<body BGCOLOR="#F5FFFA">
+<header style="background-color: darkgreen">
+    <h1 style="color: silver" align="center"> SUPERHERO FORM  </h1>
+</header>
+
+
+<?php
+
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$power = $_POST["powers"];
+
+
+echo "firstname: " . $firstname . " " . "lastname:" . " " . $lastname . " <br>";
+
+echo " Main Superhero Power:  " . " " . $power;
+
+
+?>
+
+
+
+<footer>
+    <p style="background-color: darkgreen" align="center"  >SUPERHERO Copyright(c) 2016 </p>
+</footer>
+
+
+</body>
+</html>
