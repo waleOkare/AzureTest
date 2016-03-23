b2e4a18c773037993a814bbd5fc2427e92c10337
waleokare <waleokare@outlook.com>
2016-03-23 00:36:56 +0000

    from home3

--- week7b/index.php
+++ week7b/index.php
@@ -3,15 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
 </head>
+<header>
+
+    <nav>
+
+        <ul>
+          <li>   <a href="superheroform.html"> SUPERHEROFORM </a> </li>
+           <li> <a href="battle.php">BATTLE </a> </li>
+          <li> <a href="displaybattles.php"> DISPLAY BATTLE </a> </li>
+        <li>  <a href="displaySuperheros"> DISPLAY SUPERHEROS </a>  </li>
+        </ul>
+    </nav>
+
+</header>
+
+<body bgcolor="silver">
 
 
-<body>
-
-<a href="superheroform.html"> SUPERHEROFORM </a>
-<a href="battle.php">BATTLE </a>
-<a href="displaybattles.php"> DISPLAY BATTLE </a>
-<a href="displaySuperheros"> DISPLAY SUPERHEROS </a>
 
 
 
