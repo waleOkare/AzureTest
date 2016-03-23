d1f261f4e1931afd6f623be201f0e57d6693b8f4
waleokare <waleokare@outlook.com>
2016-03-23 00:44:12 +0000

    from home3

---
+++ week7b/battle.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>battle</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body BGCOLOR="#F5FFFA">
+
+<a href="insertbattle.php"> INSERT BATTLE </a>
+
+<fieldset>
+    <form action="" method="post" >
+        <label>firstname</label> <input type="text" name="firstname" placeholder="firstname">
+        <br>
+        <label>lastname</label> <input type="text" name="lastname" placeholder="lastname">
+        <br>
+        <br>
+
+        <label> villian fought:   </label>
+        <br>
+        <br>
+        <textarea name="powers" rows = "10" cols="50" maxlength="200" ></textarea>
+        <br>
+        <input type="submit" value="Submit" style="color: darkgreen">
+    </form>
+</fieldset>
