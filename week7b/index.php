f9cdc9e6cc5daa54b84a946692d905a1ff6073c6
waleokare <waleokare@outlook.com>
2016-03-23 00:24:10 +0000

    from home3

---
+++ week7b/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+
+
+<body>
+
+<a href="superheroform.html"> SUPERHEROFORM </a>
+<a href="battle.php">BATTLE </a>
+<a href="displaybattles.php"> DISPLAY BATTLE </a>
+<a href="displaySuperheros"> DISPLAY SUPERHEROS </a>
+
+
+
+</body>
+</html>
+
+
+
+
+
--- week7b/showsuperheros.php
+++ week7b/showsuperheros.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>superheroform</title>
+    <title>showsuperheros</title>
 </head>
 <body BGCOLOR="#F5FFFA">
 <header style="background-color: darkgreen">
-    <h1 style="color: silver" align="center"> SUPERHERO FORM  </h1>
+    <h1 style="color: silver" align="center"> SUPERHERO INFORMATION  </h1>
 </header>
 
 
