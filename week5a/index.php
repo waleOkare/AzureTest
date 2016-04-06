fcc837eeaee44e0306f328950b3864c1dd5d6fa2
waleokare <waleokare@outlook.com>
2016-04-07 00:28:14 +0100

    from home3

--- week5a/index.php
+++ week5a/index.php
@@ -1,5 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Marvel Movies </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 
-echo 'Working okay';
+</head>
+
+
+<body>
+
+<header>
+    <nav>
+
+        <ul>
+            <li><a href="#"> w </a> </li>
+            <li><a href="#"> e </a>  </li>
+            <li><a href="#"> r </a> </li>
+            <li><a href="#">  t</a> </li>
+
+        </ul>
+
+    </nav>
+
+</header>
+
 
 
