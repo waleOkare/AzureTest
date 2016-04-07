1e46ec0dbe0c77895ad4fd4f39e2f714756361c7
waleokare <waleokare@outlook.com>
2016-04-07 01:17:45 +0100

    from home3

--- week5a/DisplayAllMovies.php
+++ week5a/DisplayAllMovies.php
@@ -1,4 +1,39 @@
 <?php
 include ("connection.php");
 
+echo "<br>";
 
+
+$Movies_SQL = "SELECT title
+               FROM marvelmovies
+
+";
+
+$Movies_SQL_Query = mysqli_query($db, $Movies_SQL);
+
+
+echo "<table border = '1'>";
+
+echo "<tr>";
+
+echo "<th> # </th>";
+echo "<th> Title </th>";
+
+echo "</tr>";
+
+$indx = 1;
+while($row = mysqli_fetch_array($Movies_SQL_Query, MYSQLI_ASSOC)) {
+
+    $title = $row['title'];
+
+    echo "<tr>";
+    echo "<td> $indx  </td>";
+    echo "<td> $title </td>";
+
+
+    echo "</tr>";
+
+    $indx++;
+}
+
+echo "</table>";
