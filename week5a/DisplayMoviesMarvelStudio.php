66abff8731aa329999dcf8e0273d4ab09cb9f571
waleokare <waleokare@outlook.com>
2016-04-07 01:50:13 +0100

    from home3

--- week5a/DisplayMoviesMarvelStudio.php
+++ week5a/DisplayMoviesMarvelStudio.php
@@ -34,7 +34,7 @@ echo "<br>";
 
 $Movies_SQL = "SELECT marvelMovieID, title, productionStudio
                FROM marvelmovies
-
+               WHERE productionStudio = 'Marvel Studios'
 ";
 
 $Movies_SQL_Query = mysqli_query($db, $Movies_SQL);
