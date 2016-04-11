40eb073d92410c85bdb79e5912a2d72365eb21c1
waleokare <waleokare@outlook.com>
2016-04-11 10:27:45 +0100

    cookies and sessions

--- week8/checklogin.php
+++ week8/checklogin.php
@@ -3,6 +3,9 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+$username = 'Mike';
+$password = 'mysecretpassword';
+
 
 if($username =='username' && $password=='password'){
  setcookie('access_level_cookie', 'standarduser');
