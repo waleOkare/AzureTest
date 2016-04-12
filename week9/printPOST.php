478abedff1397540ae14368f42d9f3a8e5631d85
waleokare <waleokare@outlook.com>
2016-04-12 23:53:08 +0100

    from home3

--- week9/printPOST.php
+++ week9/printPOST.php
@@ -4,4 +4,17 @@
 
 $Title = $_POST['Title'];
 $Content = $_POST['Content'];
-$option = $_POST['option'];
+$option = $_POST['Option'];
+
+
+echo '<article>';
+
+echo'<h1>' . $Title . '</h1>';
+echo '<br>';
+echo '<p>'.$Content .'</p>';
+echo '<br>';
+echo '<strong>'.$option .'</strong>';
+
+
+echo'</article>';
+
