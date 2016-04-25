caa0891e3c1cfc07e60b5faf50e827491286bd6b
waleokare <waleokare@outlook.com>
2016-04-25 09:53:53 +0100

    minimock2

---
+++ minimock/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mock Lab</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href=unsemantic-grid-responsive-tablet.css>
+
+</head>
+
+<body>
+<main class="grid-container">
+
+    <header class="grid-100">
+        <img src="logo.png" id="logo"/>
+
+        <h1 style="color: wheat"><strong> BugTracker </strong></h1>
+
+        <h3 style="color: wheat">Keeping track of all the pesky little bugs </h3>
+    </header>
+
+
+    <aside class="grid-33 mobile-grid-66">
+        <nav>
+            <ul>
+                <li><a href="showbugs.html"> Add Bugs </a></li>
+                <li><a href="showbugs.html"> All Bug Items </a></li>
+                <li><a href="showbugs.html"> Android Bugs </a></li>
+                <li><a href="showbugs.html"> iOS Bugs </a></li>
+                <li><a href="addbugs.html"> Windows Bugs </a></li>
+                <li><a href="addbugs.html"> Insert Bug </a></li>
+            </ul>
+        </nav>
+    </aside>
+
+
+
+
+    <section class="grid-66 mobile-grid-66">
+        <p>
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+            lorem ipsum dolor sit r3gfiuywbgurwhguiowhg uwor ruoewhguorwehgruwohgrwuo erwuoghuroghouwrighrw urhguwrog
+            gfiuwrhguwreghwuoerhg wuirhgwoiurghwoirghwoiughwriughw wiuhgowiurhgwroighwiorghwirg wohgowriughw4riughwoig
+
+        </p>
+    </section>
+
+
+
+</main>
+
+
+
+
+<footer class="footer">
+    <p> Designed by Wale Patrick 2016(c) </p>
+</footer>
+
+</body>
+</html>
---
+++ minimock/showbugs.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Show Bugs Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href=unsemantic-grid-responsive-tablet.css>
+
+</head>
+<body>
+<main class="grid-container">
+
+    <header class="grid-100">
+        <img src="logo.png" id="logo"/>
+
+        <h1 style="color: wheat"><strong> BugTracker </strong></h1>
+
+        <h3 style="color: wheat">Keeping track of all the pesky little bugs </h3>
+    </header>
+
+
+    <aside class="grid-33 mobile-grid-66">
+        <nav>
+            <ul>
+                <li><a href="bugs.html"> All Bug Items </a></li>
+                <li><a href="bugs.html"> Android Bugs </a></li>
+                <li><a href="bugs.html"> iOS Bugs </a></li>
+                <li><a href="bugs.html"> Windows Bugs </a></li>
+                <li><a href="bugs.html"> Insert Bug </a></li>
+            </ul>
+        </nav>
+    </aside>
+
+
+
+
+
+    <section class="grid-66 mobile-grid-66">
+
+
+
+
+
+
+
+
+
+
+
+    </section>
+
+
+
+</main>
+
+
+
+
+<footer class="footer">
+    <p> Designed by Wale Patrick 2016(c) </p>
+</footer>
+
+</body>
+</html>
