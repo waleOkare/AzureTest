095e5b401b2d95238406c42df7416c758e0b544f
waleokare <waleokare@outlook.com>
2016-04-26 00:36:14 +0100

    final part of the mock Lab

--- minimock/showbugs.php
+++ minimock/showbugs.php
@@ -50,11 +50,27 @@
             $bugName = $row['bugName'];
             $BugCategory = $row['BugCategory'];
             $BugSummary =  $row['BugSummary'];
+if(isset($_GET)){
+    $bugName = $_GET['bugName'];
+    $BugCategory = $_GET['BugCategory'];
+    $BugSummary= $_GET['BugSummary'];
 
 
-                  echo ' <h4><strong>Bug Name </strong> </h4>' .  $bugName. '<br>';
-                  echo '<h4><strong> Bug Category </strong> </h4>' . $BugCategory.  '<br>';
-                  echo '<h4><strong>Bug Summary </h4></strong>' .  $BugSummary. '<br>';
+    echo ' <h4><strong>Bug Name </strong> </h4>' .  $bugName. '<br>';
+    echo '<h4><strong> Bug Category </strong> </h4>' . $BugCategory.  '<br>';
+    echo '<h4><strong>Bug Summary </h4></strong>' .  $BugSummary. '<br>';
+
+
+}else{
+
+    echo ' <h4><strong>Bug Name </strong> </h4>' .  $bugName. '<br>';
+    echo '<h4><strong> Bug Category </strong> </h4>' . $BugCategory.  '<br>';
+    echo '<h4><strong>Bug Summary </h4></strong>' .  $BugSummary. '<br>';
+
+}
+
+
+
 
 
 
