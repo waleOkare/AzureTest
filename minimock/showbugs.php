2cc121480f4de756f63bf801c786f892b59c33ef
waleokare <waleokare@outlook.com>
2016-04-25 22:10:58 +0100

    minimock 2

--- minimock/showbugs.php
+++ minimock/showbugs.php
@@ -53,9 +53,9 @@
             $BugSummary =  $row['BugSummary'];
 
 
-                  echo ' <h4><strong>Bug Name </strong> </h4>' . $bugName . '<br>';
-                  echo '<h4><strong> Bug Category </strong> </h4>' . $BugCategory . '<br>';
-                  echo '<h4><strong>Bug Summary </h4></strong>' . $BugSummary . '<br>';
+                  echo ' <h4><strong>Bug Name </strong> </h4>' . $bugName = $_GET['bugName'] . '<br>';
+                  echo '<h4><strong> Bug Category </strong> </h4>' . $BugCategory= $_GET['BugCategory'] . '<br>';
+                  echo '<h4><strong>Bug Summary </h4></strong>' . $BugSummary= $_GET['BugSummary'] . '<br>';
 
 
 
