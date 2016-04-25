e364576e70fb8857e4fe33a2147e4ca03881202f
waleokare <waleokare@outlook.com>
2016-04-25 17:23:06 +0100

    minimock 2

---
+++ minimock/addbugs.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add Bug Page</title>
+    <link rel="stylesheet" type="text/css" href="partone/style.css">
+    <link rel="stylesheet" href=partone/unsemantic-grid-responsive-tablet.css>
+
+</head>
+
+<body>
+<main class="grid-container">
+
+    <header class="grid-100">
+        <img src="logo.png" id="logo"/>
+
+        <h1> <strong> BugTracker </strong></h1>
+
+        <h3> Keeping track of all the pesky little bugs </h3>
+    </header>
+
+
+    <aside class="grid-33 mobile-grid-66">
+        <nav>
+            <ul>
+
+                <li><a href="showbugs.html"> All Bug Items </a></li>
+                <li><a href="showbugs.html"> Android Bugs </a></li>
+                <li><a href="showbugs.html"> iOS Bugs </a></li>
+                <li><a href="addbugs.html"> Windows Bugs </a></li>
+                <li><a href="addbugs.html"> Insert Bug </a></li>
+            </ul>
+        </nav>
+    </aside>
+
+
+
+
+
+    <section class="grid-66 mobile-grid-66">
+
+
+        <form action="#" method="post">
+            <label>Bug Name</label>  <input type="text" name="BugName" required>
+            <br>
+            <br>
+            <label> Bug Summary </label> <textarea rows="8" cols="50" name="BugSummary" required> </textarea>
+            <br>
+            <br>
+            <label> Bug Category </label>
+            <select name="Option" required>
+                <option value="Android">Android</option>
+                <option value="iOS">iOS</option>
+                <option value="Windows">Windows</option>
+            </select>
+            <br>
+            <br>
+            <input type="submit" value="Submit">
+
+        </form>
+
+
+
+
+
+
+
+
+    </section>
+
+
+
+</main>
+
+
+
+
+<footer class="footer">
+    <p> Designed by Wale Patrick 2016(c) </p>
+</footer>
+
+</body>
+</html>
