8b28a4171474863d162146971f9108744783623c
waleokare <waleokare@outlook.com>
2016-05-03 00:27:33 +0100

    test files

--- week7a/test.php
+++ week7a/test.php
@@ -2,9 +2,9 @@
 
 <?php
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME', 'ba0905be48d675');
-define('DB_PASSWORD', '73cda819');
-define('DB_DATABASE', 'waleokaredb');
+define('DB_USERNAME', 'b6ec05aff05f02');
+define('DB_PASSWORD', '6889ce42');
+define('DB_DATABASE', 'CMM007ALDB-1511463');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 if ($db) {
@@ -14,6 +14,7 @@ if ($db) {
 }
 
 ?>
+
 
 DB TABLES...
 <!--
