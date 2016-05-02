6f35116ab951b30eb304adf855977a4a291ff2f8
waleokare <waleokare@outlook.com>
2016-05-02 16:11:57 +0100

    back at it, adminer seems to be working okay

---
+++ Labprep/phplab/connection.php
@@ -0,0 +1,15 @@
+<?php
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'ba0905be48d675');
+define('DB_PASSWORD', '73cda819');
+define('DB_DATABASE', 'waleokaredb');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+if ($db) {
+    echo 'Successful Connection to the database <br>';
+} else {
+    echo 'Failed to connect to the database';
+}
+
+
+?>
