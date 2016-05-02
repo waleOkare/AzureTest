9a978d2567d1de7b62710afdacc35c89e4184a3d
waleokare <waleokare@outlook.com>
2016-05-02 18:36:13 +0100

    marvel m

---
+++ Labprep/MMDB/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Landing Page </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
+
+</head>
+
+<body>
+
+<main class="grid-container">
+
+
+    <header class="grid-100">
+        <h3> MARVEL MOVIES DATABASE QUERYING </h3>
+
+    </header>
+
+
+    <aside class="grid-33 mobile-grid-66">
+        <nav>
+            <ul>
+
+                <li><a href="DisplayAllMovies.php"> All MOVIES CURRENTLY IN THE DATABASE </a></li>
+                <li><a href="DisplayMoviesMarvelStudio.php"> Android Bu</a></li>
+                <li><a href="DisplayAll2010.php"> iOS Bugs </a></li>
+                <li><a href="DisplayAllXmenFilms.php"> Windows Bugs </a></li>
+                <li><a href="index.php"> Home </a></li>
+            </ul>
+        </nav>
+    </aside>
+
+
+
+
+    <section class="grid-66 mobile-grid-66">
+
+
+        <h3> MARVEL MOVIES DATABASE QUERYING </h3>
+
+        <p> Click on Links to Navigate. </p>
+
+    </section>
+
+
+
+</main>
+
+
+
+
+<footer class="footer">
+    <p> Designed by [Wale Patrick, [2016] </p>
+</footer>
+
+</body>
+</html>
