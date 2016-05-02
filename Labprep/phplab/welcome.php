64d7eba0dbd2d6283b00acbcc75529434f0542ef
waleokare <waleokare@outlook.com>
2016-05-02 17:25:58 +0100

    login form without session

--- Labprep/phplab/welcome.php
+++ Labprep/phplab/welcome.php
@@ -19,10 +19,59 @@ $username = $_GET['username'];
 
 
 
-echo '<h4> Current user Login as:  </h4>' .$username ;
+echo "<h4> Current user Login as: {$username} </h4>";
 
 ?>
 <br>
+
+<?php
+include ("connection.php");
+
+echo "<br>";
+
+
+$user_SQL = "SELECT username, password
+               FROM users
+
+";
+
+$user_SQL_Query = mysqli_query($db, $user_SQL);
+
+echo'<h2> TABLE DISPLAYS <strong style="color: darkred"> ALL MOVIES </strong> FROM THE MARVELMOVIES DATABASE </h2>';
+echo'<br>';
+
+echo "<table border = '1'>";
+
+echo "<tr>";
+
+
+echo "<th> UserNames </th>";
+echo "<th> Passwords </th>";
+
+echo "</tr>";
+
+$indx = 1;
+while($row = mysqli_fetch_array($user_SQL_Query, MYSQLI_ASSOC)) {
+
+    $username = $row['username'];
+    $password = $row['password'];
+
+    echo "<tr>";
+
+    echo "<td>  $username  </td>";
+    echo "<td> $password </td>";
+
+
+    echo "</tr>";
+
+    $indx++;
+}
+
+echo "</table>";
+
+?>
+
+
 
 </body>
 </html>
