37a4c68e4f949e97f7f1275ca6bb54b4e9a265f0
waleokare <waleokare@outlook.com>
2016-05-02 20:55:33 +0100

    marvel m

--- Labprep/MMDB/DisplayAllMovies.php
+++ Labprep/MMDB/DisplayAllMovies.php
@@ -1,7 +1,106 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1511463
- * Date: 02/05/2016
- * Time: 18:21
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Home  </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href=unsemantic-grid-responsive-tablet.css>
+
+</head>
+
+<body>
+
+<main class="grid-container">
+
+    <header class="grid-100">
+
+
+        <h1> MARVEL MOVIES DATABASE </h1>
+
+
+    </header>
+
+
+    <aside class="grid-33 mobile-grid-66">
+
+        <nav>
+            <ul>
+                <li><a href="index.php"> Home </a></li>
+                <li><a href="DisplayAllMovies.php"> All MOVIES  </a></li>
+                <li><a href="DisplayMoviesMarvelStudio.php">  MOVIES BY MARVEL STUDIOS </a></li>
+                <li><a href="DisplayAfter2010.php"> MOVIES FROM 2010 </a></li>
+                <li><a href="DisplayAllXmenFilms.php"> ALL XMEN FILMS </a></li>
+
+            </ul>
+        </nav>
+
+    </aside>
+
+
+
+
+
+    <section class="grid-66 mobile-grid-66">
+
+
+
+        <?php
+        include("connection.php");
+
+
+        $sql = "
+              SELECT *
+              FROM marvelmovies
+              ;";
+
+
+        echo "<table border = '1'>";
+
+        echo "<tr>";
+
+
+
+
+        echo "<th> Title </th>";
+        echo "</tr>";
+
+        $result = mysqli_query($db,$sql);
+        while($row = $result->fetch_array()){
+
+
+            $title = $row['title'];
+
+            echo "<tr>";
+
+
+            echo "<td> $title </td>";
+
+
+            echo "</tr>";
+
+
+        }
+
+
+
+        echo '</table>';
+
+        ?>
+
+
+    </section>
+
+
+
+</main>
+
+
+
+
+<footer id="footer">
+    <p> Designed by [Wale Patrick, [2016] </p>
+</footer>
+
+</body>
+</html>
+
--- Labprep/MMDB/DisplayMoviesMarvelStudio.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1511463
- * Date: 02/05/2016
- * Time: 18:22
- */
