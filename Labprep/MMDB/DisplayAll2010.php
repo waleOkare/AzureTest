e1cecd4c005dc7a9cba1d1e33e47a5663f6f48a9
waleokare <waleokare@outlook.com>
2016-05-02 18:52:27 +0100

    marvel m

--- Labprep/MMDB/DisplayAll2010.php
+++ Labprep/MMDB/DisplayAll2010.php
@@ -1,7 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Landing Page </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
+
+</head>
+
+<body>
+
+<main class="grid-container">
+
+
+    <header class="grid-100">
+        <h3> MARVEL MOVIES DATABASE QUERYING </h3>
+
+    </header>
+
+
+    <aside class="grid-33 mobile-grid-66">
+        <nav>
+            <ul>
+                <li><a href="index.php"> Home </a></li>
+                <li><a href="DisplayAllMovies.php"> All MOVIES CURRENTLY IN THE DATABASE </a></li>
+                <li><a href="DisplayMoviesMarvelStudio.php"> ALL MARVEL CREATED BY MARVEL STUDIOS </a></li>
+                <li><a href="DisplayAll2010.php"> MOVIES FROM 2010 </a></li>
+                <li><a href="DisplayAllXmenFilms.php"> ALL XMEN FILMS </a></li>
+
+            </ul>
+        </nav>
+    </aside>
+
+
 <?php
-/**
- * Created by PhpStorm.
- * User: 1511463
- * Date: 02/05/2016
- * Time: 18:22
- */
+include("connection.php");
+
+
+$sql = "
+              SELECT *
+              FROM marvelmovies
+              WHERE yearReleased>2010;
+
+";
+
+
+$result = mysqli_query($db,$sql);
+while($row = $result->fetch_array()){
+
+    $yearReleased = $row['yearReleased'];
+    $title = $row['title'];
+    $productionStudio = $row['productionStudio'];
+    $notes = $row['notes'];
+
+   echo  $yearReleased ;
+    echo   $title ;
+    echo  $productionStudio ;
+    echo  $notes;
+}
+
--- Labprep/MMDB/index.php
+++ Labprep/MMDB/index.php
@@ -22,16 +22,15 @@
     <aside class="grid-33 mobile-grid-66">
         <nav>
             <ul>
-
-                <li><a href="DisplayAllMovies.php"> All MOVIES CURRENTLY IN THE DATABASE </a></li>
-                <li><a href="DisplayMoviesMarvelStudio.php"> Android Bu</a></li>
-                <li><a href="DisplayAll2010.php"> iOS Bugs </a></li>
-                <li><a href="DisplayAllXmenFilms.php"> Windows Bugs </a></li>
                 <li><a href="index.php"> Home </a></li>
+                <li><a href="DisplayAllMovies.php"> All MOVIES CURRENTLY IN THE DATABASE </a></li>
+                <li><a href="DisplayMoviesMarvelStudio.php"> ALL MARVEL CREATED BY MARVEL STUDIOS </a></li>
+                <li><a href="DisplayAll2010.php"> MOVIES FROM 2010 </a></li>
+                <li><a href="DisplayAllXmenFilms.php"> ALL XMEN FILMS </a></li>
+
             </ul>
         </nav>
     </aside>
-
 
 
 
@@ -40,7 +39,7 @@
 
         <h3> MARVEL MOVIES DATABASE QUERYING </h3>
 
-        <p> Click on Links to Navigate. </p>
+        <p> Click on Links to Navigate.... </p>
 
     </section>
 
