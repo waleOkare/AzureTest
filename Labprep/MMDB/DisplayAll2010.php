24272c38bb80fc5fef0dd952d8e0d4cdf1d7210b
waleokare <waleokare@outlook.com>
2016-05-02 19:09:13 +0100

    marvel m

--- Labprep/MMDB/DisplayAll2010.php
+++ Labprep/MMDB/DisplayAll2010.php
@@ -36,55 +36,47 @@
 
 
 
-
     <section class="grid-66 mobile-grid-66">
 
+<?php
+include("connection.php");
 
-        <?php
-        include("connection.php");
 
-
-        $sql = "
+$sql = "
               SELECT *
               FROM marvelmovies
               WHERE yearReleased>2010;
 
 ";
-        echo "<table border = '1'>";
+echo "<table border = '1'>";
 
-        echo "<tr>";
-
-
-
-        echo "<th> yearReleased </th>";
-        echo "<th> Title </th>";
-
-
-        echo "</tr>";
+echo "<tr>";
 
 
 
-        $result = mysqli_query($db,$sql);
-        while($row = $result->fetch_array()){
-
-            $yearReleased = $row['yearReleased'];
-            $title = $row['title'];
-
-            echo "<tr>";
-
-            echo "<td> $yearReleased </td>";
-            echo "<td> $title </td>";
+echo "<th> yearReleased </th>";
+echo "<th> Title </th>";
 
 
-            echo "</tr>";
-        }
-
-
-        ?>
+echo "</tr>";
 
 
 
+$result = mysqli_query($db,$sql);
+while($row = $result->fetch_array()){
 
+    $yearReleased = $row['yearReleased'];
+    $title = $row['title'];
+
+    echo "<tr>";
+
+    echo "<td> $yearReleased </td>";
+    echo "<td> $title </td>";
+
+
+    echo "</tr>";
+}
+?>
 
     </section>
 
@@ -96,7 +88,7 @@
 
 
 <footer class="footer">
-    <p> Designed by [Wale Patrick, 2016] </p>
+    <p> Designed by [Wale Patrick, [2016] </p>
 </footer>
 
 </body>
