cf437bb669639fc5c8f428dde8ac1085e5ae7563
waleokare <waleokare@outlook.com>
2016-05-02 16:32:14 +0100

    login form without session

---
+++ Labprep/phplab/welcome.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+
+
+
+<body>
+
+
+<h1> Whew! </h1>
+
+
+
+</body>
+</html>
+
