fdce3fd9900fcb0f6b034943e9c0fe1f0664fd64
waleokare <waleokare@outlook.com>
2016-05-02 16:29:31 +0100

    login form without session

--- Labprep/phplab/index.php
+++ Labprep/phplab/index.php
@@ -9,11 +9,13 @@
     <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
-<h1>PHP Login Form with Session</h1>
+<h1>PHP Login Form without Session</h1>
 <div class="loginBox">
     <h3>Login Form</h3>
     <br><br>
+
     <form method="post" action="login.php">
+
         <label>Username:</label><br>
         <input type="text" name="username" placeholder="username"/>
             <br><br>
@@ -21,6 +23,7 @@
         <input type="password" name="password" placeholder="password" />
         <br><br>
         <input type="submit" name="submit" value = "login"/>
+
     </form>
 
     <div class="error"><?php //echo $error;?><?php //echo $username; echo
