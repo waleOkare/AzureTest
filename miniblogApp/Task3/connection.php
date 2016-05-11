3762e7942da4064fba45220d73604d066ec5e526
waleokare <waleokare@outlook.com>
2016-05-11 16:17:51 +0100

    My Blog App from home

--- miniblogApp/Task3/connection.php
+++ miniblogApp/Task3/connection.php
@@ -1,27 +1,15 @@
-
-<!--- This is the connection file to the Sql database. -->
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME', 'b6ec05aff05f02');
-define('DB_PASSWORD', '6889ce42');
-define('DB_DATABASE', 'CMM007ALDB-1511463');
+define('DB_USERNAME', 'ba0905be48d675');
+define('DB_PASSWORD', '73cda819');
+define('DB_DATABASE', 'waleokaredb');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 if ($db) {
-
-  //  echo "Successful Connection to the database <br>"  ;
+//    echo 'Successful Connection to the database <br>';
 } else {
-
-    echo "Failed to connect to the database";
+    echo 'Failed to connect to the database';
 }
-
-
-
 
 ?>
 
